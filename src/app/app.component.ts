00b038a3c9681a3a1d5d1525dcfbe2ae30563118
Torin <torinsamuelson@yahoo.com>
2021-10-27 17:00:05 -0500

    Implement character-card component core functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'research-project';
+
+    title = 'research-project';
+
+    characters: Character[] = [
+        new Character('bloodhound', 'Recon',     'Technological Tracker'),
+        new Character('gibraltar',  'Defensive', 'Shielded Fortress'),
+        new Character('lifeline',   'Support',   'Combat Medic'),
+        new Character('pathfinder', 'Recon',     'Forward Scout'),
+        new Character('wraith',     'Offensive', 'Interdimensional Skirmisher'),
+        new Character('bangalore',  'Offensive', 'Professional Soldier'),
+        new Character('caustic',    'Defensive', 'Toxic Trapper'),
+        new Character('mirage',     'Offensive', 'Holographic Trickster'),
+        new Character('octane',     'Offensive', 'High-Speed Daredevil'),
+        new Character('wattson',    'Defensive', 'Static Defender'),
+        new Character('crypto',     'Recon',     'Surveillance Expert'),
+        new Character('revenant',   'Offensive', 'Synthetic Nightmare'),
+        new Character('loba',       'Support',   'Translocating Thief'),
+        new Character('rampart',    'Defensive', 'Amped Modder'),
+        new Character('horizon',    'Offensive', 'Gravitational Manipulator'),
+        new Character('fuse',       'Offensive', 'Bombastic Explosives Expert'),
+        new Character('valkyrie',   'Recon',     'Winged Avenger'),
+        new Character('seer',       'Recon',     'Ambush Artist')
+    ];
+
+}
+
+class Character {
+
+    name: string;
+    banner: string;
+    icon: string;
+    title: string;
+
+    constructor(name: string, type: string, title: string) {
+        this.name = name;
+        this.banner = `../../assets/Images/Banners/${name}.png`;
+        this.icon = `../../assets/Images/Icons/${type}.png`;
+        this.title = title;
+    }
+
 }
--- src/app/character-card/character-card.component.ts
+++ src/app/character-card/character-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'character-card',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterCardComponent implements OnInit {
 
-  constructor() { }
+    @Input("banner") characterImage?: string; 
+    @Input("icon") characterIcon?: string;
+    @Input("name") characterName?: string;
+    @Input("title") characterTitle?: string;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
